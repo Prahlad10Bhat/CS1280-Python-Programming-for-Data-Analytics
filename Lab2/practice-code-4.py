b47f5ba4a8841044b0c2418fe4caf73e8af220ad
Prahlad10Bhat <prahlad10bhat@gmail.com>
2025-01-31 11:11:43 +0530

    Create practice-code-4.py

---
+++ Lab2/practice-code-4.py
@@ -0,0 +1,9 @@
+def print_parameters(a, b, c):
+  print("1st param:", a)
+  print("2nd param:", b)
+  print("3rd param:", c)
+
+a = input("Enter 1st param: ")
+b = input("Enter 2nd param: ")
+c = input("Enter 3rd param: ")
+print_parameters(a, b, c)
